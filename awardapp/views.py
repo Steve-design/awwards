1cd8a2830414d18b15893e3be1475006a595cafa
Steve-design <smzalendo31@gmail.com>
2019-11-25 09:29:27 +0300

    create a homepage function

--- awardapp/views.py
+++ awardapp/views.py
@@ -15,3 +15,14 @@ from rest_framework import status
 from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
+def homepage(request):
+    posts = Post.all_posts()
+    profile = Profile.get_all_profiles()
+    ratings=Ratings.objects.all()
+    current_user = request.user
+    context =  {
+        "profile": profile,
+        "posts":posts ,
+        "ratings":ratings,
+    }
+    return render(request, 'index.html', context)
