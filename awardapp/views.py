029235b1bbe3efccb12d5527e51424d9d8e09c77
Steve-design <smzalendo31@gmail.com>
2019-11-25 09:27:48 +0300

    import required modules

--- awardapp/views.py
+++ awardapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import datetime as dt
+from django.http import HttpResponse,Http404,HttpResponseRedirect, JsonResponse
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from .forms import *
+from .email import *
+from django.contrib.auth.decorators import login_required
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+from rest_framework import status
+from .permissions import *
+from .serializer import *
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
