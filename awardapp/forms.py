163545861da2c09cb28482b4b55ded126ffc5f2c
Steve-design <smzalendo31@gmail.com>
2019-11-25 09:56:01 +0300

    add the RatingForm class

--- awardapp/forms.py
+++ awardapp/forms.py
@@ -4,3 +4,8 @@ from .models import *
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name', max_length=30)
     email = forms.EmailField(label='Email')
+
+class RatingsForm(forms.ModelForm):
+    class Meta:
+        model = Ratings
+        exclude = ['post_rated', 'score']    
