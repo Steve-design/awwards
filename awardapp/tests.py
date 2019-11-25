152a94299d243afffd9424b266e65f425e5df9b7
Steve-design <smzalendo31@gmail.com>
2019-11-25 14:21:44 +0300

    add tests for posts

--- awardapp/tests.py
+++ awardapp/tests.py
@@ -21,3 +21,26 @@ class ProfileTest(TestCase):
         self.profile.save()
         profile = Profile.find_profile('Steve')
         self.assertTrue(len(profile) > 0)
+
+class PostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='steve')
+        self.profile = Profile.objects.create(user = self.user,bio = 'my sample bio',contact='mycontact')
+
+        self.post= Post.objects.create(name='sample_post',
+                                       image='images/', description='desc',
+                                       live_link='http://url' )
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post,Post))
+
+    def test_all_posts(self):
+        self.post.save()
+        post = Post.all_posts()
+        self.assertTrue(len(post) > 0)
+
+    def test_search_post(self):
+        self.post.save()
+        post = Post.search_by_name('sample_post')
+        self.assertTrue(len(post) > 0)        
