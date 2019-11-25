f9e4213f14c6e1cd86615571d53a2e8c112c4af1
Steve-design <smzalendo31@gmail.com>
2019-11-25 08:35:57 +0300

    create a model for profile

--- awardapp/models.py
+++ awardapp/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 import datetime as dt
 # Create your models here.
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='images/', blank=True)
+    bio = HTMLField()
+    contact = HTMLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
