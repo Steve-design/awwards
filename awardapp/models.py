884a1b2b79963c2cda676af47a2814f2c2a94aa4
Steve-design <smzalendo31@gmail.com>
2019-11-25 08:38:37 +0300

    add post model

--- awardapp/models.py
+++ awardapp/models.py
@@ -34,3 +34,12 @@ class Profile(models.Model):
 
     class Meta:
         ordering = ['user']    
+
+class Post(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/', blank=True)
+    description = HTMLField(blank=True)
+    live_link=models.URLField(blank=True)
+    profile=models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    user_profile = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts',blank=True)
+    date = models.DateTimeField(auto_now_add=True)        
