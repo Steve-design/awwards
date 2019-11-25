e5f19f88a644048e01347ec8940f7a23174be3bb
Steve-design <smzalendo31@gmail.com>
2019-11-25 08:49:35 +0300

    configure urls

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -2,3 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
+
+urlpatterns=[
+    url(r'^api/posts/$', views.Postlist.as_view()),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^$', views.homepage, name='homepage'),
+    url(r'^upload$', views.add_post, name='add'),
+    url(r'^accounts/profile/(?P<username>\w+)', views.profile, name='my_profile'),
+    url(r'^post/(\d+)', views.get_individual_post, name='post'),
+    url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
+    url(r'^rate/(?P<pk>\d+)', views.rate_post, name='rate_post'),
+    url(r'^new/profile$', views.add_profile, name='add_profile'),
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
