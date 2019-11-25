245eabd8eaea39d207a642cb4796f23b82e3c059
Steve-design <smzalendo31@gmail.com>
2019-11-25 14:09:43 +0300

    add profile tests

--- awardapp/tests.py
+++ awardapp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 from .models import *
-# Create your tests here.
+
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='Steve')
+        self.profile = Profile.objects.create(user = self.user,bio = 'loves coding', contact='0745697834')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_profile(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_get_all_profiles(self):
+        self.profile.save()
+        profile = Profile.get_all_profiles()
+        self.assertTrue(len(profile) > 0)
+
+    def test_find_profile(self):
+        self.profile.save()
+        profile = Profile.find_profile('Steve')
+        self.assertTrue(len(profile) > 0)
